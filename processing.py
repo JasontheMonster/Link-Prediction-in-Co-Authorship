9d7901b58c5c51b2a46e86687f266df21e0b86d4
limqiying <limqiying19@gmail.com>
2017-12-06 23:17:52 -0500

    processing done

--- processing.py
+++ processing.py
@@ -1,79 +1,51 @@
 """
-Grabs data from MAG and processes it
+Grabs data from Microsoft Academic Graph API and data processing
 """
 
 import json
-from unidecode import unidecode
 import pickle
 import requests
 import itertools
-
-write = True
-read = True
-
-
-def generate_authors(read, write):
-    """
-    generate a list of authors, for strings formatted to remove accents and special characters
-    """
-    if write:
-        with open('dblp_coauthorship.json', 'r') as f:
-            d = json.load(f)
-
-        authors = set()
-        for author1, author2, year in d:
-            author1 = unidecode(author1)
-            author2 = unidecode(author2)
-            authors.update([author1.replace('.', '').replace('-', '').lower(),
-                            author2.replace('.', '').replace('-', '').lower()])
-
-        with open('authors', 'wb') as fp:
-            pickle.dump(list(authors), fp)
-
-    if read:
-        with open('authors', 'rb') as fp:
-            item_list = pickle.load(fp)
-            return item_list
+from collections import defaultdict
+from time import sleep
 
 
 def get_json():
-    url = "https://westus.api.cognitive.microsoft.com/academic/v1.0/evaluate?expr=" \
+    """
+    send request to Microsoft Academic Graph for information on papers on fluid dynamics
+    generates a json file
+    """
+    url1 = "https://westus.api.cognitive.microsoft.com/academic/v1.0/evaluate?expr=" \
           "Composite(F.FN=='fluid dynamics')&model=latest&count=10000&offset=0&attributes=Y,Ti,Id,F.FN,AA.AuN,W"
     headers = {
         "Ocp-Apim-Subscription-Key": ""  # add own key
     }
     path = "./database/"
-    # for i, url in enumerate(url_list):
-    r = requests.get(url, headers=headers)
+    r = requests.get(url1, headers=headers)
     data = r.json()
-    # file_name = path + str(i) + "_author_attributes.json"
     file_name = "fluid_dynamics.json"
     with open(path+file_name, 'w') as f:
         json.dump(data, f)
-
-
-def build_url():
-    with open('authors', 'rb') as fp:
-        author_list = pickle.load(fp)
-    url_list = []
-    for i in range(0, len(author_list), 50):
-        s = []
-        for author in author_list[i:i+50]:
-            a = "AA.AuN=='%s'" % author
-            s.append(a)
-        s1 = ",".join(s)
-        s2 = "Composite(OR(%s))" % s1
-        s3 = "https://westus.api.cognitive.microsoft.com/academic/v1.0/evaluate?expr=%s&model=latest" \
-               "&count=1000&offset=0&attributes=W,Ti,AA.AuN,F.FN,Y HTTP/1.1" % s2
-        url_list.append(s3)
-    return url_list
+    url2 = "https://westus.api.cognitive.microsoft.com/academic/v1.0/evaluate?expr=" \
+           "Composite(F.FN=='fluid dynamics')&model=latest&count=10000&offset=1000&attributes=Y,Ti,Id,F.FN,AA.AuN,W"
+    headers = {
+        "Ocp-Apim-Subscription-Key": ""  # add own key
+    }
+    r = requests.get(url2, headers=headers)
+    data = r.json()
+    file_name = "fluid_dynamics2.json"
+    with open(path + file_name, 'w') as f:
+        json.dump(data, f)
 
 
 def create_edge_list():
+    """
+    creates and pickles the edges list and vertex set
+    """
+    edge_list = set()
+    authors = set()
     with open('./database/fluid_dynamics.json', 'rb') as fp:
         db = json.load(fp)['entities']
-        edge_list = set()
-        authors = set()
         for paper in db:
             author_list = [a["AuN"] for a in paper["AA"]]
             edges = list(itertools.combinations(author_list, 2))
@@ -81,10 +53,92 @@ def create_edge_list():
             edge_year = list(zip(edges, year))
             authors.update(author_list)
             edge_list.update(edge_year)
+    with open('./database/fluid_dynamics2.json', 'rb') as fp:
+        db2 = json.load(fp)['entities']
+        for paper in db2:
+            author_list = [a["AuN"] for a in paper["AA"]]
+            edges = list(itertools.combinations(author_list, 2))
+            year = [paper['Y']]*len(edges)
+            edge_year = list(zip(edges, year))
+            authors.update(author_list)
+            edge_list.update(edge_year)
+
     with open("database/edges.p", 'wb') as f:
         pickle.dump(edge_list, f)
     with open("database/authors.p", 'wb') as f:
         pickle.dump(authors, f)
 
 
+# Ti,AA.AuN,W,F.FN
+def node_attributes(authorlist=None):
+    """
+    for every author in the author list, gets information on the number of collaborations, list of keywords,
+    list of fields he/she has worked in, and number of papers published
+    creates a json file to store data
+    """
+    if authorlist is None:
+        with open("./database/train_authors.p", 'rb') as f:
+            authors = pickle.load(f)
+    else:
+        authors = authorlist
+
+    with open('./database/attributes.json', 'r') as f:
+        attributes = json.load(f)
+    counter = 0
+    for i, author in enumerate(authors):
+        if author in attributes.keys():
+            print(str(author) + " was skipped because info was already found")
+            continue
+        counter += 1
+        url = "https://westus.api.cognitive.microsoft.com/academic/v1.0/calchistogram?expr=Composite(AA.AuN=='%s')" \
+              "&model=latest&attributes=Ti,AA.AuN,W,F.FN&count=100&offset=0" % author
+        headers = {
+            "Ocp-Apim-Subscription-Key": ""  # add own key
+        }
+        r = requests.get(url, headers=headers)
+        print("round " + str(i) + " r: " + str(r))
+        d = r.json()
+        data = d['histograms']
+        attributes[author] = dict()
+        for entry in data:
+            if entry["attribute"] == "Ti":
+                num_papers = entry["distinct_values"]
+                attributes[author]["num_papers"] = num_papers
+            elif entry["attribute"] == "AA.AuN":
+                num_collaborations = entry["distinct_values"]
+                attributes[author]["num_collaborations"] = num_collaborations
+            elif entry["attribute"] == "W":
+                keywords = list(set([e["value"] for e in entry["histogram"]]))
+                attributes[author]["keywords"] = keywords
+            else:
+                assert entry["attribute"] == "F.FN", "attribute type is not field"
+                fields = list(set([e["value"] for e in entry["histogram"]]))
+                attributes[author]["fields"] = fields
+        if counter == 20:
+            print("writing to file")
+            counter = 0
+            with open("database/attributes.json", 'w') as f:
+                json.dump(attributes, f)
+        sleep(10)
+
+    with open("database/attributes.json", 'w') as f:
+        json.dump(attributes, f)
+
+
+def get_edge_weights():
+    """
+    for each edge, the weight is the number of collaborations done between the two authors
+    """
+    with open("database/edges.p", 'rb') as f1:
+        edges = pickle.load(f1)
+
+    edge1 = [x[0] for x in edges]
+    weights = defaultdict(lambda: 0)
+    for edge in edge1:
+        weights[edge] += 1
+
+    w = dict(weights)
+
+    with open("database/weights.p", 'wb') as f:
+        pickle.dump(w, f)
 
