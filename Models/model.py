7623ca9c1f24b286c8fba5ad8a344e5c20793baf
Shikun Wang <jasonwangsk@yahoo.com>
2017-12-08 20:06:06 -0500

    histogram

--- Models/model.py
+++ Models/model.py
@@ -9,6 +9,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier
+from matplotlib import pyplot as plt
+
 class Model(object):
 
 	def __init__(self, train_data, train_labels, test_data, test_labels):
@@ -20,15 +22,15 @@ class Model(object):
 
 	def random_forest(self):
 
-		clf = RandomForestClassifier(max_depth=2, random_state=0)
+		clf = RandomForestClassifier(n_estimators= 30, max_depth=3, random_state=0, min_samples_leaf=10)
 		#clf = svm.SVC()
 		clf.fit(self.train_data, self.train_labels)
-		print(clf.feature_importances_)
 		y_pred = []
 		for query in tqdm(self.test_data):
 			y_pred.append(clf.predict([query])[0])
 
 		print (confusion_matrix(self.test_labels, y_pred))
+		return clf.feature_importances_
 
 
 	def decision_Tree(self):
@@ -99,7 +101,17 @@ if __name__ == "__main__":
 	train_labels = pickle.load( open ("labels.p", "rb"))
 	train_data = pickle.load( open ("data_matrix.p", "rb"))
 	model = Model(train_data, train_labels, test_data, test_labels)
-	model.svm_kernel()
+	importance = model.random_forest()
+	fig = plt.figure()
+
+	y_pos = range(len(importance))
+	objects = ["P", "CN", "K",
+	 "F", "SK", "SN"]
+	plt.bar(y_pos, importance, align='center', alpha=0.5)
+	plt.xticks(y_pos, objects)
+	plt.ylabel('Importance Score')
+	plt.title('Different Features')
+	fig.savefig("importances3.png")
 
 
 
